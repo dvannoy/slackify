1cbb3d743db9303966fa64e8f747a074434910c2
dvannoy <dustinvannoy@gmail.com>
2017-09-29 11:02:28 -0700

    Fixes to dates and code cleanup

--- slack_runner.py
+++ slack_runner.py
@@ -1,5 +1,4 @@
 #!usr/bin/env python
-import datetime
 import arrow
 import json
 import requests
@@ -31,7 +30,6 @@ def get_payload(date, rec):
     date2, weekly_message, scripture, memory_verse, q1, q2, application, prayer_message, prayer, quote = rec
     dt = date.format('MMMM D, YYYY')
     day_of_week = date.weekday() + 1
-    print day_of_week
     if day_of_week == 1:
         log.info("Processing payload for Monday")
         return monday(get_channel(day_of_week), username, dt, weekly_message, scripture)
@@ -45,7 +43,7 @@ def get_payload(date, rec):
         return friday(get_channel(day_of_week), username, prayer, prayer_message, quote)
 
 def monday(channel, username, date, weekly_msg, scripture):
-    payload = {  # 'text': msg,
+    payload = {
         'username': username,
         'mrkdwn_in': True,
         'channel': channel,
@@ -78,7 +76,7 @@ def tuesday(channel, username, memory_verse):
 
 
 def wednesday(channel, username, q1, q2):
-    payload = {  # 'text': msg,
+    payload = {
         'username': username,
         'mrkdwn_in': True,
         'channel': channel,
@@ -98,7 +96,7 @@ def thursday(channel, username, application_message):
     msg = ("It's time to put what we've studied into action.  See our application challenge and try to think up your "
     "own to post for others.  Respond to the group if you have a good story of how an application worked out "
     "in your life.")
-    payload = {  # 'text': msg,
+    payload = {
         'username': username,
         'mrkdwn_in': True,
         'channel': channel,
@@ -115,7 +113,11 @@ def thursday(channel, username, application_message):
 
 
 def friday(channel, username, prayer, pray_for, quote):
-    payload = {  # 'text': msg,
+    if quote != '':
+        quote_str = '_' + quote + '_'
+    else:
+        quote_str = quote
+    payload = {
         'username': username,
         'mrkdwn_in': True,
         'channel': channel,
@@ -123,7 +125,7 @@ def friday(channel, username, prayer, pray_for, quote):
             {
                 "title": "Prayer",
                 "text": "{0}\n\nThis week pray for: _{1}_".format(prayer, pray_for),
-                "pretext": "_{0}_\n\n".format(quote),
+                "pretext": "{0}\n\n".format(quote_str),
                 "mrkdwn_in": ["text", "pretext"]
             }
         ]
@@ -135,36 +137,26 @@ def read_source(filename):
     file_data = []
     log.debug("Reading text from file %s", filename)
     with open(filename, 'rb') as f:
-        file_reader = csv.reader(f)
+        file_reader = csv.reader(f.read().splitlines())
         next(file_reader, None) # skip header
         for row  in file_reader:
-            #log.debug("Loading row with values %s", row)
             file_data.append(row)
     return file_data
 
 
 if __name__ == "__main__":
-
-    today = datetime.datetime.today()
-    weekday = today.weekday()
-    #now = datetime.datetime.now().hour
-    log.debug("Running with weekday %d and date %s", weekday, today)
-
     records = read_source("/Users/dustinvannoy/dev/data/SlackersDevotionalSchedule.csv")
-    print records
+    log.debug("Data from file: %s", records)
 
-    curr_week = '2017-05-01'
+    now = arrow.now('US/Pacific')
+    weekday = now.weekday()
+    weekstart = now.shift(days=-weekday).floor('day') #.format('YYYY-MM-DD') #.format('M/D/YY')
+    today = now.floor('day')
+    log.info("Current day: %s, weekday: %s, week start: %s", today, weekday, weekstart)
 
     for rec in records:
-        if rec[0] is not None and rec[0] == curr_week:
-            #date = arrow.get(rec[0], 'YYYY-MM-DD')
-            #date = arrow.get('2017-08-14', 'YYYY-MM-DD')
-            date = arrow.get('2017-08-18', 'YYYY-MM-DD')
-            log.info(date)
-            payload = get_payload(date, rec)
+        if rec[0] is not None and rec[0] != '' and arrow.get(rec[0], 'M/D/YY', tzinfo='US/Pacific') == weekstart:
+            payload = get_payload(today, rec)
             log.info("Payload received: %s", payload)
-            # TODO: Refactor
-            dt = date.format('MMMM D, YYYY')
-            day_of_week = date.weekday() + 1
             if not TEST_MODE:
                 post_to_slack(payload, slack_webhook)
